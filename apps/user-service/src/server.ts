f4fd83d118efe3b15b3a3bc6947c08a35c354262
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-09-15 00:00:16 +0300

    fix: add swagger ui for api documentaions

---
+++ apps/user-service/src/server.ts
@@ -0,0 +1,35 @@
+
+import type { Application } from 'express';
+import { createRequire } from 'node:module';
+import cors from "cors";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import userRoutes from "./routes/user.js"
+import authRoutes from "./routes/auth.js"
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+
+const app: Application = express()
+
+app.use(cors())
+app.use(express.json())
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Qo\'annaa API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./apps/user-service/src/routes/*.ts'], 
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/v1/users", userRoutes)
+app.use("/v1/auth", authRoutes)
+export default app
