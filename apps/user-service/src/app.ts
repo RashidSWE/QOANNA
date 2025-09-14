3b2903e6c6e7c5bda9117b47b7f861540f3b35c7
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-09-14 23:59:34 +0300

    fix: add dotenv to load environment variables

--- apps/user-service/src/app.ts
+++ apps/user-service/src/app.ts
@@ -1,16 +1,8 @@
-import type { Application, Request, Response } from 'express'
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-
-const express = require('express');
-
-
-const app: Application = express();
+import 'dotenv/config';
+import app from "./server.js";
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from the user Service!');
-});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
