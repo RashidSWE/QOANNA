a9284922ce504768cef898f69dd3e7b4d154491c
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-10-19 17:52:42 +0300

    setup: setup jest for tests

---
+++ apps/user-service/jest.setup.js
@@ -0,0 +1,3 @@
+process.env.JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || "test-private-key";
+process.env.JWT_PUBLIC_KEY = process.env.JWT_PUBLIC_KEY || "test-public-key";
+process.env.DATABASE_URL = process.env.DATABASE_URL
